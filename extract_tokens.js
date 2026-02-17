e2ba31f8cdeeb5060f0ead35a761608fd381fde1
Sai Nikhil Thirandas <saint.math.1729@gmail.com>
2026-02-17 00:15:30 -0800

    chore: commit remaining workspace changes

--- extract_tokens.js
+++ extract_tokens.js
@@ -2,7 +2,7 @@
 // Copy the output and paste into your .env file
 
 const clientId = '6hr71icfdda6n67uvvm3nvlu4d';
-const username = 'AmazonFederateOIDC_saintamz';
+const username = 'AmazonFederateOIDC_<amazon_user_alias>';  // Replace <amazon_user_alias> with your Amazon user alias (e.g. ajassy)
 
 const idToken = localStorage.getItem(`CognitoIdentityServiceProvider.${clientId}.${username}.idToken`);
 const refreshToken = localStorage.getItem(`CognitoIdentityServiceProvider.${clientId}.${username}.refreshToken`);
